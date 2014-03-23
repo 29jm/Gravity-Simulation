b7bbd0fb93ee11c2a14f60b51c8c4d4787d1381b
Johan Manuel <johan.manuel@live.fr>
2014-03-23 16:43:58 +0100

    changed mass from float to int

--- Graviy-Simulation/Universe.hpp
+++ Graviy-Simulation/Universe.hpp
@@ -2,18 +2,20 @@
 #define UNIVERSE_HPP
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "Body.hpp"
 
 class Universe
 {
 public:
-	void addPlanet(sf::Vector2i position, float m, sf::Vector2f dir);
+	void addPlanet(sf::Vector2f position, float m, sf::Vector2f dir);
+	void addPlanet(Body p);
+
 	void move(float delta_t);
 	void draw(sf::RenderWindow& window);
 	void eraseAt(const sf::Vector2f& pos);
 
 private:
-
 	std::vector<Body> planets;
 };
 
